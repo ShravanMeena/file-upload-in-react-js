0b1e3e36773ece3c1651887a22d92bd7bfc26e5d
shravanmeena <shravanmeena47@gmail.com>
2022-07-13 22:59:55 +0530

    file upload added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import axios from "axios";
+
+const endpoint = "http://localhost:8000/api/file-upload"; //your endpoint
 
 function App() {
+  const onSubmit = async (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    formData.append("type", "category"); //any other properties
+
+    // USING FETCH
+    const res = await fetch(endpoint, {
+      method: "POST",
+      body: formData,
+      headers: {
+        token: "coll",
+      },
+    }).then((res) => res.json());
+
+    console.log(res);
+
+    // USING AXIOS
+    // await axios({
+    //   method: 'post',
+    //   url: endpoint,
+    //   data:formData,
+    //   headers:{
+    //     "Content-Type":'multipart/form-data',
+    //     "token":"12345"
+    //   }
+    // })
+    //   .then(function (response) {
+    //     console.log(response)
+    //   }).catch(function(error) {
+    //     console.log(error);
+    //   })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" name="file" onChange={onSubmit} />
     </div>
   );
 }
